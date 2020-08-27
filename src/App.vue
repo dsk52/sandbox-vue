bdaee0ace8a507c753bcf3fa3257510d30c313a8
Daisuke KONISHI <skydai1151@gmail.com>
2020-08-27 10:00:16 +0900

    add layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>Hello {{ name }}!</div>
+  <div>
+    <Container>
+      <div class="wrapper">
+        <main class="main">
+          aaa
+        </main>
+        <aside class="sidebar">
+          bbb
+        </aside>
+      </div>
+    </Container>
+  </div>
 </template>
 
 <script>
+  import Container from './layouts/Container'
+
   export default {
-    data() {
-      return {
-        name: "Vue",
-      };
-    },
+    components: {
+      Container
+    }
   };
 </script>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .main {
+    width: 70%;
+  }
+
+  .sidebar {
+    width: 30%;
+  }
+</style>
---
+++ src/layouts/Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Container"
+}
+</script>
+
+<style scoped>
+  .container {
+    max-width: 1024px;
+    width: 100%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
